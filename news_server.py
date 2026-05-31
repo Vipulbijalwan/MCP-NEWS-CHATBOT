900aebbd2a50c041593fa33dd198ad9f7120f457
Vipul Bijalwan <vipulbijalwan112@gmail.com>
2026-05-31 15:41:53 +0530

    server

--- news_server.py
+++ news_server.py
@@ -36,7 +36,5 @@ def ping():
 
 if __name__ == "__main__":
     mcp.run(
-        transport="streamable-http",
-        host="0.0.0.0",
-        port=8000
+        transport="streamable-http"
     )
